6a073e7cd8a8f85914bca0e6843eba0ff1f17c11
kubaadamcik <jakubadamcik11@gmail.com>
2025-10-02 10:26:39 +0200

    Made counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-function App() {
-    return <div className="container">
-        <h1 className="text-primary"></h1>
+import {useState} from "react";
 
+function App() {
+    const handleClick = () => setCislo(cislo + 1);
+
+    const [cislo, setCislo] = useState(0)
+
+
+
+    return <div className="container">
+        <div className="row">
+            <div className="col">
+                <div>
+                    <h1 className="text-primary">Počítadlo</h1>
+                    <button className="btn-primary" onClick={handleClick}>Klikni na mě</button>
+                    <p className="text-secondary">{cislo}</p>
+                </div>
+            </div>
+        </div>
     </div>;
 }
 
