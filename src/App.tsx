763f64533d33b834960a3e0596be88993bbba4e4
kubaadamcik <jakubadamcik11@gmail.com>
2025-10-10 08:35:31 +0200

    qwqr

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import {useState} from "react";
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import { useState } from "react";
 
 function App() {
-    const handleClick = () => setCislo(cislo + 1);
+  const handleClick = () => setCislo(cislo + 1);
 
-    const [cislo, setCislo] = useState(0)
+  const [cislo, setCislo] = useState(0);
 
-
-
-    return <div className="container">
-        <div className="row">
-            <div className="col">
-                <div>
-                    <h1 className="text-primary">Počítadlo</h1>
-                    <button className="btn-primary" onClick={handleClick}>Klikni na mě</button>
-                    <p className="text-secondary">{cislo}</p>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div>
+            <h1 className="text-primary">Počítadlo</h1>
+            <button className="btn-primary" onClick={handleClick}>
+              Klikni na mě
+            </button>
+            <p className="text-secondary">{cislo}</p>
+          </div>
         </div>
-    </div>;
+      </div>
+    </div>
+  );
 }
 
 export default App;
